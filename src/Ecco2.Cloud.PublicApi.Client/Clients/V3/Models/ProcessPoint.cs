be024270bbe5b71f041bd61a6e27874a3b1e26fc
Beat Ackermann <beat@ecco2.ch>
2023-12-04 14:11:16 +0100

    First work: interface definition for DataBroker

---
+++ src/Ecco2.Cloud.PublicApi.Client/Clients/V3/Models/ProcessPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+
+namespace Ecco2.Cloud.PublicApi.Client.V3;
+
+/// <summary>
+/// Represents a data point on the ECCO2 data broker.
+/// </summary>
+public class ProcessPoint
+{
+	/// <summary>
+	/// The agreed-upon, globally unique identifier of the process point.
+	/// </summary>
+	[JsonPropertyName("id")]
+	public string Identifier { get; set; }
+
+	/// <summary>
+	/// The value of the process point.
+	/// </summary>
+	[JsonPropertyName("value")]
+	public double? Value { get; set; }
+
+	/// <summary>
+	/// The quality tag to be applied to the value.
+	/// </summary>
+	[JsonPropertyName("quality")]
+	public Quality Quality { get; set; }
+	
+	/// <summary>
+	/// The time stamp of the value, in UTC.
+	/// </summary>
+	[JsonPropertyName("timeStamp")]
+	public DateTime TimeStamp { get; set; }
+}
